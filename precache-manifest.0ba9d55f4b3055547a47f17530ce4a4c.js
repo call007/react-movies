dfa5b6576b4d8cd770237eb506676ba884fa96c2
call007 <e.shevtsov@unitedtraders.com>
2020-04-12 11:49:02 +0300

    Updates

---
+++ precache-manifest.0ba9d55f4b3055547a47f17530ce4a4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254c642e30745b9339e30630a6141415",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "a98efa3e250e26e405bd",
+    "url": "/react-movies/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "37a772488133b0ef6169",
+    "url": "/react-movies/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a98efa3e250e26e405bd",
+    "url": "/react-movies/static/js/2.293c80c5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-movies/static/js/2.293c80c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a772488133b0ef6169",
+    "url": "/react-movies/static/js/main.c63ff33b.chunk.js"
+  },
+  {
+    "revision": "50522ec578f881bc26b5",
+    "url": "/react-movies/static/js/runtime-main.cf97ac22.js"
+  }
+]);
